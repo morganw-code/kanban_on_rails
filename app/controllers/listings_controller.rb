45d43dafec5d70e26040e84d9a62b539fee944b3
Morgan Webb <mnbwebb2233@gmail.com>
2020-04-28 16:42:12 +1000

    Implement listing update and edit crud operations and some basic css styling

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,7 @@
 class ListingsController < ApplicationController
     before_action :authenticate_user!
+    # call set_user_listing only when edit, update, or destroy action has been called
+    before_action :set_user_listing, only: [:edit, :update, :destroy]
 
     def index
         @listings = Listing.all
@@ -19,9 +21,35 @@ class ListingsController < ApplicationController
         @listing = Listing.new
     end
 
+    def edit
+        @listing = Listing.find(params[:id])
+    end
+
+    def show
+    
+    end
+
+    def update
+        @listing = Listing.update(params[:id], listing_params)
+        if @listing.errors.any?
+            render plain: "errors"
+        else 
+            redirect_to listings_path
+        end
+    end
+
     private
 
     def listing_params
         return params.require(:listing).permit(:title, :message)
+    end
+
+    def set_user_listing
+        @listing = current_user.listings.find_by_id(params[:id])
+
+        # if listing doesn't exist redirect user to listings_path
+        if @listing == nil
+            redirect_to listings_path
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "/listings", to: "listings#index", as: "listings"
   post "/listings", to: "listings#create"
   get "/listings/new", to: "listings#new", as: "new_listing"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
+  get "/listings/:id", to: "listings#show", as: "listing"
+  patch "/listings/:id", to: "listings#update"
 end
